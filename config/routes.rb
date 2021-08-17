3622705c02afd6891f403e6f4d117dbc7d2ccdce
Drew Jennings <andrew_jennings@ymail.com>
2021-08-17 12:21:35 +0100

    each task is now a link to a show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # read all
   get 'tasks', to: 'tasks#index'
+  # read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
